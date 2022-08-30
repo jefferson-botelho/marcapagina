545f844e9c9d59a2c312a0c9c60b7f53eb2a0d18
Jefferson Botelho <jefferson.andrade@camara.leg.br>
2022-08-30 05:37:49 -0300

    Commit inicial.

---
+++ src/main/java/com/marcapagina/MarcapaginaApplication.java
@@ -0,0 +1,13 @@
+package com.marcapagina;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarcapaginaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarcapaginaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marcapagina/MarcapaginaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marcapagina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarcapaginaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
